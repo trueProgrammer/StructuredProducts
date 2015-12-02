4d20dc429ae37ed8f4d7d1baf349ac7f680f0c5a
Vlad <vl.isaev@mail.ru>
2015-12-02 22:43:32 +0300

    Init commit

---
+++ src/main/java/com/structuredproducts/SpitterService.java
@@ -0,0 +1,16 @@
+package com.structuredproducts;
+
+
+import java.util.Random;
+
+/**
+ * Created by Vlad on 22.11.2015.
+ */
+public class SpitterService {
+
+
+    public static String RANDOM  = Long.toString(new Random().nextLong());
+
+    public SpitterService(){}
+
+}
