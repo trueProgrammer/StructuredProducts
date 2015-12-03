a63a0562e834810c4fbec79efa0f75b78a20485d
Vlad <vl.isaev@mail.ru>
2015-12-04 00:49:38 +0300

    Site under construction 2

---
+++ src/main/java/com/structuredproducts/rest/Data.java
@@ -0,0 +1,17 @@
+package com.structuredproducts.rest;
+
+/**
+ * Created by Vlad on 03.12.2015.
+ */
+public class Data {
+
+    private final String name;
+
+    public Data(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
