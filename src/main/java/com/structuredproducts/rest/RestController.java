f914446620c707464285648db79b76972ebdc9d5
Vlad <vl.isaev@mail.ru>
2015-12-04 14:13:59 +0300

    Site under construction finish

--- src/main/java/com/structuredproducts/rest/RestController.java
+++ src/main/java/com/structuredproducts/rest/RestController.java
@@ -1,5 +1,6 @@
 package com.structuredproducts.rest;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/v1/data")
 public class RestController {
 
+    private final static Logger logger = Logger.getLogger(RestController.class);
+
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<Data> getData() {
+        logger.trace("Get data");
+        logger.error("Get data");
         return new ResponseEntity<>(new Data("test"), HttpStatus.OK);
     }
 
---
+++ src/main/webapp/resources/js/controller.js
@@ -0,0 +1,7 @@
+angular.module('app',[])
+    .controller('main', function($scope, $http) {
+        var url = "api/v1/data";
+        $http.get(url).success(function(data) {
+            $scope.message = data.name;
+        })
+    })
