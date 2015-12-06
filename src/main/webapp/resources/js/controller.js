3c448258eec45802bf04024a2e5b3c824cf45ae1
Vlad <vl.isaev@mail.ru>
2015-12-07 00:01:49 +0300

    Created site prototype

--- src/main/webapp/resources/js/controller.js
+++ src/main/webapp/resources/js/controller.js
@@ -1,7 +1,45 @@
-angular.module('app',[])
+var app = angular.module('app',['ngRoute'])
     .controller('main', function($scope, $http) {
         var url = "api/v1/data";
         $http.get(url).success(function(data) {
             $scope.message = data.name;
         })
-    });
+    })
+    .controller('main', [function() {
+        angular.element(document).ready(function () {
+            App.init();
+            jQuery("#layerslider").layerSlider({
+                navStartStop: false,
+                skin: 'fullwidth',
+                responsive : true,
+                responsiveUnder : 960,
+                layersContainer : 960,
+                autoPlayVideos: true,
+                skinsPath: 'resources/assets/plugins/layer-slider/layerslider/skins/'
+            });
+            });
+    }]);
+
+    app.config(['$routeProvider',
+       function($routeProvider) {
+         $routeProvider.
+            when('/', {
+              templateUrl: 'views/template/main.html',
+              controller: 'main'
+            }).
+           when('/about', {
+             templateUrl: 'views/template/about.html',
+             controller: 'main'
+           }).
+           when('/contacts', {
+            templateUrl: 'views/template/contacts.html',
+            controller: 'main'
+            }).
+            when('/partners', {
+                templateUrl: 'views/template/partners.html',
+                controller: 'main'
+            }).
+           otherwise({
+             redirectTo: '/'
+           });
+       }]);
