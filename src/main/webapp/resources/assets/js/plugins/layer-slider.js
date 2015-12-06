bf10bd63e8eb8c0ede71c7a801fdc7234de7f3c8
Vlad <vl.isaev@mail.ru>
2015-12-06 15:13:46 +0300

    Added from template

---
+++ src/main/webapp/resources/assets/js/plugins/layer-slider.js
@@ -0,0 +1,21 @@
+var LayerSlider = function () {
+
+    return {
+
+        //Layer Slider
+        initLayerSlider: function () {
+		    $(document).ready(function(){
+		        jQuery("#layerslider").layerSlider({
+			        skin: 'fullwidth',
+			        responsive : true,
+			        responsiveUnder : 960,
+			        layersContainer : 960,
+					navStartStop: false,
+					autoPlayVideos: true,
+			        skinsPath: 'assets/plugins/layer-slider/layerslider/skins/'
+			    });
+		    });     
+        }
+
+    };
+}();        
