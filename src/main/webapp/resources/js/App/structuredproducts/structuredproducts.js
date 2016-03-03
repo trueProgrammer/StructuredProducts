86a81ae25193583aa8430ac92637d50fea82c850
Vlad <vl.isaev@mail.ru>
2016-03-04 00:47:07 +0300

    moved css files + changed about structured products page

--- src/main/webapp/resources/js/App/structuredproducts/structuredproducts.js
+++ src/main/webapp/resources/js/App/structuredproducts/structuredproducts.js
@@ -2,6 +2,36 @@ angular.module('App.structuredproducts')
     .config(['$routeProvider',
         function($routeProvider){
             $routeProvider.when('/structuredproducts', {
-                templateUrl: 'resources/js/App/structuredproducts/structuredproducts.html'
+                templateUrl: 'resources/js/App/structuredproducts/structuredproducts.html',
+                controller: 'structuredproductsCtrl'
             })
-        }]);
+        }])
+.controller('structuredproductsCtrl', [ '$scope',
+    function($scope) {
+
+        var selected = {};
+
+        $scope.select = function (id, tab) {
+            if (selected[id] === undefined) {
+                selected[id] = tab;
+            } else {
+                if (selected[id] === tab) {
+                    selected[id] = undefined;
+                } else {
+                    selected[id] = tab;
+                }
+            }
+        };
+
+        $scope.isSelected = function (id, tab) {
+            if (selected[id] === undefined) {
+                return false;
+            } else {
+                if( selected[id] === tab ) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+    }]);
