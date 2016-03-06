280e94fdb871d4a8b9bfb131821386a6da0c8214
Aleksandr Chermashentsev <aleksandr.chermashentchev@gmail.com>
2016-03-06 13:49:38 +0300

    Create basic markdown for product page Add Chart.js library

--- src/main/webapp/resources/js/App/product/product.js
+++ src/main/webapp/resources/js/App/product/product.js
@@ -8,6 +8,35 @@
          }])
 .controller('product', ['$scope', '$log', 'restService',
          function($scope, $log, restService) {
-             var chart = new Chart("#chart");
-             chart.drawChartOxis(800, 300, 5, 15);
+             $scope.product = {};
+             $scope.product.name = 'Нефть';
+             $scope.product.risk = 'Высокий';
+             $scope.product.return = '100%';
+             var data = {
+                 labels: ["January", "February", "March", "April", "May", "June", "July"],
+                 datasets: [
+                     {
+                         label: "My First dataset",
+                         fillColor: "rgba(220,220,220,0.2)",
+                         strokeColor: "rgba(220,220,220,1)",
+                         pointColor: "rgba(220,220,220,1)",
+                         pointStrokeColor: "#fff",
+                         pointHighlightFill: "#fff",
+                         pointHighlightStroke: "rgba(220,220,220,1)",
+                         data: [65, 59, 80, 81, 56, 55, 40]
+                     },
+                     {
+                         label: "My Second dataset",
+                         fillColor: "rgba(151,187,205,0.2)",
+                         strokeColor: "rgba(151,187,205,1)",
+                         pointColor: "rgba(151,187,205,1)",
+                         pointStrokeColor: "#fff",
+                         pointHighlightFill: "#fff",
+                         pointHighlightStroke: "rgba(151,187,205,1)",
+                         data: [28, 48, 40, 19, 86, 27, 100]
+                     }
+                 ]
+             };
+             var ctx = document.getElementById("chart").getContext("2d");
+             var chart = new Chart(ctx).Line(data);
          }]);
