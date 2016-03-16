1e7b31f15f50d6baa0dc408cabb2523f74250955
aleksandr_chermashentsev <aleksandr_chermashentsev@epam.com>
2016-03-16 13:31:05 +0300

    Add possibility to add new hex Draw hex in two row

--- src/main/webapp/resources/js/App/createproduct/createproduct.js
+++ src/main/webapp/resources/js/App/createproduct/createproduct.js
@@ -1,82 +1,94 @@
 angular.module('App.createproduct')
 
-.config(['$routeProvider',
-    function($routeProvider){
-        $routeProvider.when('/createproduct', {
-            templateUrl: 'resources/js/App/createproduct/createproduct.html',
-            controller: 'createproduct'
-        })
-    }])
-.controller('createproduct', [ '$scope', '$log',
-        function($scope, $log) {
+    .config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.when('/createproduct', {
+                templateUrl: 'resources/js/App/createproduct/createproduct.html',
+                controller: 'createproduct'
+            })
+        }])
+    .controller('createproduct', ['$scope', '$log',
+        function ($scope, $log) {
 
-    $scope.profitSaved = false;
-    $scope.profitLine = "";
-    $scope.profits = ['10%', '20%', '30%', '40%'];
-    $scope.fromProfit = $scope.profits[0];
-    $scope.toProfit = $scope.profits[0];
+            $scope.profitSaved = false;
+            $scope.profitLine = "";
+            $scope.profits = ['10%', '20%', '30%', '40%'];
+            $scope.fromProfit = $scope.profits[0];
+            $scope.toProfit = $scope.profits[0];
 
-    $scope.setFromProfit = function(value) {
-        $scope.fromProfit = value;
-    };
-    $scope.setToProfit = function(value) {
-        $scope.toProfit = value;
-    };
+            $scope.setFromProfit = function (value) {
+                $('#profit').text('От ' + value + ' до ' + $scope.toProfit);
+                $scope.fromProfit = value;
+            };
+            $scope.setToProfit = function (value) {
+                $('#profit').text('От ' + $scope.fromProfit + ' до ' + value);
+                $scope.toProfit = value;
+            };
 
-    $scope.saveProfit = function() {
-        if($scope.fromProfit <= $scope.toProfit) {
-            $scope.profitSaved = true;
-            $scope.profitLine = " от " + $scope.fromProfit + "   до " + $scope.toProfit;
-        }
-    };
+            $scope.saveProfit = function () {
+                if ($scope.fromProfit <= $scope.toProfit) {
+                    $scope.profitSaved = true;
+                    $scope.profitLine = " от " + $scope.fromProfit + "   до " + $scope.toProfit;
+                }
+            };
 
-    var defaultParams = [{
-        text: 'Доходность',
-        stroke: '#91CF50'
-    }, {
-        text: 'Уровень риска',
-        stroke: '#FDBF01'
-    },{
-        text: '+ параметр',
-        stroke: '#BEBEBE',
-        id: 'addParam1',
-        onclick: 'onParamClick()'
-    }, {
-        text: 'Сумма вложений',
-        stroke: '#FD0001'
-    },{
-        text: 'Срок вложений',
-        stroke: '#4774AA'
-    }, {
-        text: '+ параметр',
-        stroke: '#BEBEBE',
-        id: 'addParam2',
-        onclick: 'onParamClick()'
-    }];
+            var defaultParams = [{
+                text: 'Доходность',
+                stroke: '#91CF50',
+                id: 'profit'
+            }, {
+                text: 'Уровень риска',
+                stroke: '#FDBF01',
+                id: 'riskLevel'
+            }, {
+                text: '+ параметр',
+                stroke: '#BEBEBE',
+                id: 'addParam1'
+            }, {
+                text: 'Сумма вложений',
+                stroke: '#FD0001',
+                id: 'sum'
+            }, {
+                text: 'Срок вложений',
+                stroke: '#4774AA',
+                id: 'term'
+            }, {
+                text: '+ параметр',
+                stroke: '#BEBEBE',
+                id: 'addParam2'
+            }, {
+                text: 'Валюта',
+                stroke: '#FDBF01',
+                id: 'currency'
+            }];
 
-    var optParams = [{
-        text: 'Тип продукта',
-        stroke: '#91CF50'
-    }, {
-        text: 'Валюта',
-        stroke: '#FDBF01'
-    },{
-        text: 'Размер выплат',
-        stroke: '#4774AA',
-    }, {
-        text: 'Тип базового актива',
-        stroke: '#FDBF01'
-    },{
-        text: 'Риски',
-        stroke: '#4774AA'
-    }, {
-        text: 'Периодичность выплат',
-        stroke: '#FDBF01',
-    }, {
-        text: 'Стратегия',
-        stroke: '#4774AA',
-    }];
+            var optParams = [ {
+                text: 'Размер выплат',
+                stroke: '#4774AA',
+                id: 'returnValue'
+            }, {
+                text: 'Тип базового актива',
+                stroke: '#FDBF01',
+                id: 'baseActiveType'
+            },{
+                text: 'Тип продукта',
+                stroke: '#91CF50',
+                id: 'type'
+            }, {
+                text: 'Риски',
+                stroke: '#4774AA',
+                id: 'risks'
+            }, {
+                text: 'Периодичность выплат',
+                stroke: '#FDBF01',
+                id: 'paymentsPeriod'
+            }, {
+                text: 'Стратегия',
+                stroke: '#4774AA',
+                id: 'strategy'
+            }];
 
-    new hexParams({radius: 72, defaultParams: defaultParams, optParams: optParams});
+            new hexParams({radius: 72, defaultParams: defaultParams, optParams: optParams});
 
-}]);
+            $('#profit').text('От ' + $scope.fromProfit + ' до ' + $scope.toProfit);
+        }]);
--- src/main/webapp/resources/lib/hexParams.js
+++ src/main/webapp/resources/lib/hexParams.js
@@ -1,4 +1,5 @@
 (function () {
+
     hexParams = function (config) {
         this.defaultParams = config.defaultParams;
         this.optParams = config.optParams;
@@ -10,25 +11,39 @@
             .append("feGaussianBlur")
             .attr("stdDeviation", 0);
 
+        this.mainGroup = this.svg.append('g')
+            .attr('id', 'main')
+            .attr('filter', 'url(#blur)');
+        this.addGroup = this.svg.append('g')
+            .attr('id', 'addParam');
+
         var hexAngles = d3.range(Math.PI / 6, 2 * Math.PI + Math.PI / 6, Math.PI / 3);
+        var hexAngles2 = d3.range(Math.PI / 3, 2 * Math.PI + Math.PI / 3, Math.PI / 3);
         var radius = config.radius;
 
         var self = this;
 
-        window.onAddParamClick = function() {
-            self.optParams.splice(0,1);
-            prepareDataForLinear(self.optParams);
-            self.svg.select('#addParam')
-                .selectAll("text")
-                .data(self.optParams)
-                .exit()
+        function onAddParamClick(data) {
+            var removeIndex = self.optParams.map(function(item) { return item.id; })
+                .indexOf(data.id);
+
+            self.defaultParams.push(self.optParams.splice(removeIndex, 1)[0]);
+
+            self.addGroup
+                .selectAll("*")
                 .remove();
-        };
-        window.onParamClick = function () {
+            self.mainGroup
+                .selectAll("*")
+                .remove();
+            self.filter.attr('stdDeviation', 0);
+            prepareData(self.defaultParams);
+            drawHex(self.defaultParams, self.mainGroup, onParamClick);
+        }
+        function onParamClick() {
             self.filter.attr('stdDeviation', 5);
             prepareDataForLinear(self.optParams);
-            drawHex(self.optParams, null, 'addParam');
-        };
+            drawHex(self.optParams, self.addGroup, onAddParamClick);
+        }
 
         var prepareDataForLinear = function(params) {
             params[0].x = radius + 10;
@@ -50,30 +65,34 @@
             params[0].x = self.svg[0][0].clientWidth / 2;
             params[0].y = self.svg[0][0].clientHeight / 2;
             params[0].angle = hexAngles[0];
+            var angles = hexAngles;
+            var radiusPow = 2;
 
             for (var i = 1; i < params.length; i++) {
-                params[i].x = params[0].x + Math.sin(params[i - 1].angle) * radius * 2;
-                params[i].y = params[0].y - Math.cos(params[i - 1].angle) * radius * 2;
-                params[i].angle = hexAngles[i % hexAngles.length];
+                if (i == 7) {
+                    angles = hexAngles2;
+                    radiusPow = 3.5;
+                    params[i].x = params[0].x;
+                    params[i].y = params[0].y - radius * 3.5;
+                    params[i].angle = angles[0];
+                    continue;
+                }
+                params[i].x = params[0].x + Math.sin(params[i - 1].angle) * radius * radiusPow;
+                params[i].y = params[0].y - Math.cos(params[i - 1].angle) * radius * radiusPow;
+                params[i].angle = angles[i % angles.length];
             }
         };
-        var drawHex = function(params, filter, groupId) {
-            //Draw hex
-            self.svg.append('g')
-                .attr('id', groupId)
-                .attr('filter', filter)
+        var drawHex = function(params, group, onClick) {
+
+            var innerGroups = group
+                .append('g')
                 .selectAll('.hexagon')
-                .data(params)
-                .enter().append("path")
-                .attr("class", "hexagon")
+                .data(params, function(d) {return d.id})
+                .enter();
+
+            var paths = innerGroups.append("path")
                 .attr("d", function (d) {
                     return "M" + d.x + "," + d.y + self.hexbin.hexagon();
-                })
-                .attr('onclick', function (d) {
-                    return d.onclick;
-                })
-                .attr('id', function (d) {
-                    return d.id;
                 })
                 .attr("stroke-width", "5px")
                 .attr('stroke', 'white')
@@ -84,11 +103,15 @@
                 })
                 .duration(1000);
 
+            paths.each(function(path) {
+                var pathDiv = d3.select(this);
+                pathDiv.on('click', function(path) {
+                    onClick.call(pathDiv, path);
+                })
+            });
+
             //Draw texts
-            self.svg.selectAll('#' + groupId)
-                .selectAll("text")
-                .data(params)
-                .enter()
+            innerGroups
                 .append("text")
                 .attr("x", function (d) {
                     return d.x;
@@ -106,10 +129,28 @@
                 })
                 .transition()
                 .duration(1000);
+            innerGroups
+                .append("text")
+                .attr("x", function (d) {
+                    return d.x;
+                })
+                .attr("y", function (d) {
+                    return d.y + 20;
+                })
+                .attr('id', function (d) {
+                    return d.id;
+                })
+                .attr("class","hex-style")
+                .attr("class","hex-text")
+                .attr('onclick', function (d) {
+                    return d.onclick;
+                })
+                .transition()
+                .duration(1000);
 
         };
         prepareData(this.defaultParams);
-        drawHex(this.defaultParams, 'url(#blur)', 'main');
+        drawHex(this.defaultParams, this.mainGroup, onParamClick);
 
     }
 })();
