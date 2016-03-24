4ee695888f3985e9eddb6c705921da6a9f24d3e1
aleksandr_chermashentsev <aleksandr_chermashentsev@epam.com>
2016-03-24 15:09:10 +0300

    Create awesome phone input Add validation to form

--- src/main/webapp/resources/js/App/createproduct/createproduct.js
+++ src/main/webapp/resources/js/App/createproduct/createproduct.js
@@ -257,31 +257,46 @@ angular.module('App.createproduct')
                 });
 
                 modalInstance.rendered.then(function () {
-                    /*$("#phone").mask("(999) 999-9999");*/
+                    $("#phone").mask("+7 (999) 999-9999");
                 }, function () {
                 });
             };
 
-            $scope.sendParams = function() {
-                if ($scope.sendform.$valid) {
-
-                    alert(JSON.stringify(savedControls));
-                }
-            }
 
         }])
         .controller('sendRequestCtrl', ['$scope', 'restService',
         function ($scope, restService) {
+            var checkPhone = function() {
+                var phoneEl = $('#phone');
+                var val = phoneEl.val();
+                if (val && val !== '') {
+                    return true;
+                }
+                phoneEl.addClass('invalid-control');
+                return false;
+            };
             $scope.sendParams = function() {
-                if ($scope.sendform.$valid) {
+                var phoneCheck = checkPhone();
+                if ($scope.sendform.$valid && phoneCheck) {
                     $scope.user.controls = savedControls;
                     restService.createProductRequest($scope.user,
                     function() {
+                        $scope.invalidFormMessage = "";
+                        $('input').removeClass('invalid-control');
                         $scope.msg = "Ваша заявка успешно отправлена. Ожидайте звонка от брокера.";
                     },
                         function() {
+                            $scope.invalidFormMessage = "";
+                            $('input').removeClass('invalid-control');
                             $scope.msg = "Проблема при обработке заявки."
                         })
+                } else {
+                    $scope.invalidFormMessage = "Пожалуйста, заполните все необходимые поля";
+                    if ($scope.sendform.$error && $scope.sendform.$error.required) {
+                        $scope.sendform.$error.required.forEach(function(req) {
+                            $("input[name='" +req.$name +"']").addClass('invalid-control')
+                        });
+                    }
                 }
             }
 
